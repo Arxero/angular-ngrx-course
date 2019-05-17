ca3029d1c0d1163a29296d586e6da03e20ed163b
Maverick Cloud <admin@syntrwave.com>
2019-05-18 01:10:22 +0300

    How To Access and Use the NgRx Store Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import {Observable} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
 import { AppState } from './reducers';
 import { Logout } from './auth/auth.actions';
+import { map } from 'rxjs/operators';
+import { AuthState } from './auth/auth.reducer';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
     constructor(private store: Store<AppState>) {
-
     }
 
     ngOnInit() {
+        this.isLoggedIn$ = this.store.pipe(
+            map((state: { auth: AuthState }) => state.auth.loggedIn)
+        )
 
+        this.isLoggedOut$ = this.store.pipe(
+            map((state: { auth: AuthState }) => !state.auth.loggedIn)
+        )
 
     }
 
