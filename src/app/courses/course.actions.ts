15b3f0ddad7a2251e0fa22e51b783b40e5fc73c4
Maverick Cloud <admin@syntrwave.com>
2019-05-18 18:21:28 +0300

    Load All Courses - Action Design and Implementation

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -1,22 +1,37 @@
 import { Action } from "@ngrx/store";
 import { Course } from "./model/course";
 
-
+//action name | source of the action | event that was triggered
 export enum CourseActionTypes {
     CourseRequested = '[View Course Page] Course Requested',
     CourseLoaded = '[Courses API] Course Loaded',
+    AllCoursesRequested = '[Courses Home Page] All Courses Requested',
+    AllCoursesLoaded = '[Courses API] All Courses Loaded',
 }
 
+//single course actions
 export class CourseRequested implements Action {
     readonly type = CourseActionTypes.CourseRequested;
-
     constructor(public payload: { courseId: number }) { }
 }
 
 export class CourseLoaded implements Action {
     readonly type = CourseActionTypes.CourseLoaded;
-
-    constructor(public payload: { course: Course}) {}
+    constructor(public payload: { course: Course }) { }
 }
 
-export type CourseActions = CourseRequested | CourseLoaded;
+//all courses actions
+export class AllCoursesRequested implements Action {
+    readonly type = CourseActionTypes.CourseRequested;
+}
+
+export class AllCoursesLoaded implements Action {
+    readonly type = CourseActionTypes.CourseRequested;
+    constructor(public payload: { courses: Course[] }) { }
+}
+
+export type CourseActions =
+    CourseRequested |
+    CourseLoaded |
+    AllCoursesRequested |
+    AllCoursesLoaded;
