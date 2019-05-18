aa56cbf8d01eb62a3e049b91edb5675d4025285b
Maverick Cloud <admin@syntrwave.com>
2019-05-18 17:05:20 +0300

    Implementing a Router Resolver using NgRx Store

--- src/app/courses/services/course.resolver.ts
+++ src/app/courses/services/course.resolver.ts
@@ -3,8 +3,11 @@ import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/r
 import { Course } from "../model/course";
 import { Observable } from "rxjs";
 import { CoursesService } from "./courses.service";
-import { Store } from "@ngrx/store";
+import { Store, select } from "@ngrx/store";
 import { AppState } from "../../reducers";
+import { selectCourseById } from "../course.selectors";
+import { CourseRequested } from "../course.actions";
+import { tap, filter, first } from "rxjs/operators";
 
 
 
@@ -19,7 +22,22 @@ export class CourseResolver implements Resolve<Course> {
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
         let courseId = route.params['id'];
-        return this.coursesService.findCourseById(courseId);
+        //return this.coursesService.findCourseById(courseId);
+
+        //we are quering/asking the store if there is such course in there
+        return this.store.pipe(
+            select(selectCourseById(courseId)),
+
+            //handling when course is not present in the store
+            tap(course => {
+                if (!course) {
+                    this.store.dispatch(new CourseRequested({ courseId }))
+                }
+            }),
+            //what happens if the course is not yet avaiable to the store
+            filter(course => !!course),
+            first()
+        )
     }
 
 }
