a721a780d78134834458228f8818dd1003e2a510
Maverick Cloud <admin@syntrwave.com>
2019-05-18 18:10:16 +0300

    Implementing an NgRx Entity Reducer, NgRx Entity In Action (Demo)

--- src/app/courses/course.reducers.ts
+++ src/app/courses/course.reducers.ts
@@ -1,5 +1,6 @@
 import { Course } from "./model/course";
 import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
+import { CourseActions, CourseActionTypes } from "./course.actions";
 
 
 
@@ -8,3 +9,16 @@ export interface CoursesState extends EntityState<Course> {
 }
 
 export const adapter: EntityAdapter<Course> = createEntityAdapter<Course>();
+
+export const initialCoursesState: CoursesState = adapter.getInitialState();
+
+export function coursesReducer(state = initialCoursesState, action: CourseActions): CoursesState {
+    switch (action.type) {
+        case CourseActionTypes.CourseLoaded:
+            return adapter.addOne(action.payload.course, state)
+            
+    
+        default:
+            return state
+    }
+}
