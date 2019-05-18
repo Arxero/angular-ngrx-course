767c8c3831347a2d165554bc6ede8393afe21779
Maverick Cloud <admin@syntrwave.com>
2019-05-18 16:11:36 +0300

    NgRx Entity - Section Kick Off

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {MatPaginator, MatTableDataSource} from "@angular/material";
-import {Course} from "../model/course";
-import {CoursesService} from "../services/courses.service";
-import {debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
-import {merge, fromEvent} from "rxjs";
-import {LessonsDataSource} from "../services/lessons.datasource";
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { MatPaginator, MatTableDataSource } from "@angular/material";
+import { Course } from "../model/course";
+import { CoursesService } from "../services/courses.service";
+import { debounceTime, distinctUntilChanged, startWith, tap, delay } from 'rxjs/operators';
+import { merge, fromEvent } from "rxjs";
+import { LessonsDataSource } from "../services/lessons.datasource";
 
 
 @Component({
@@ -14,39 +14,28 @@ import {LessonsDataSource} from "../services/lessons.datasource";
     styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit, AfterViewInit {
-
-    course:Course;
-
+    course: Course;
     dataSource: LessonsDataSource;
-
-    displayedColumns= ["seqNo", "description", "duration"];
-
+    
+    displayedColumns = ["seqNo", "description", "duration"];
     @ViewChild(MatPaginator) paginator: MatPaginator;
 
-
     constructor(private route: ActivatedRoute,
-                private coursesService: CoursesService) {
-
+        private coursesService: CoursesService) {
     }
 
     ngOnInit() {
-
         this.course = this.route.snapshot.data["course"];
-
         this.dataSource = new LessonsDataSource(this.coursesService);
-
         this.dataSource.loadLessons(this.course.id, 0, 3);
-
     }
 
     ngAfterViewInit() {
-
         this.paginator.page
-        .pipe(
-            tap(() => this.loadLessonsPage())
-        )
-        .subscribe();
-
+            .pipe(
+                tap(() => this.loadLessonsPage())
+            )
+            .subscribe();
     }
 
     loadLessonsPage() {
@@ -55,6 +44,4 @@ export class CourseComponent implements OnInit, AfterViewInit {
             this.paginator.pageIndex,
             this.paginator.pageSize);
     }
-
-
 }
--- src/app/courses/services/course.resolver.ts
+++ src/app/courses/services/course.resolver.ts
@@ -1,23 +1,25 @@
-
-
-
-import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {CoursesService} from "./courses.service";
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { CoursesService } from "./courses.service";
+import { Store } from "@ngrx/store";
+import { AppState } from "../../reducers";
 
 
 
 @Injectable()
 export class CourseResolver implements Resolve<Course> {
 
-    constructor(private coursesService:CoursesService) {
+    constructor(
+        private coursesService: CoursesService,
+        private store: Store<AppState>) {
 
     }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
-        return this.coursesService.findCourseById(route.params['id']);
+        let courseId = route.params['id'];
+        return this.coursesService.findCourseById(courseId);
     }
 
 }
