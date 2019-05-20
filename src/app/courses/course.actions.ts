ed0e7a1bd136bff2fdfd89f4cc75562eca46c30f
Maverick Cloud <admin@syntrwave.com>
2019-05-20 15:04:49 +0300

    Lessons Pagination Feature - NgRx Action Design

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -1,14 +1,20 @@
 import { Action } from "@ngrx/store";
 import { Course } from "./model/course";
 import { Update } from "@ngrx/entity";
+import { Lesson } from "./model/lesson";
 
 //action name | source of the action | event that was triggered
 export enum CourseActionTypes {
     CourseRequested = '[View Course Page] Course Requested',
     CourseLoaded = '[Courses API] Course Loaded',
+
     AllCoursesRequested = '[Courses Home Page] All Courses Requested',
     AllCoursesLoaded = '[Courses API] All Courses Loaded',
-    CourseSaved = '[Edit Course Dialog] Course Saved'
+    CourseSaved = '[Edit Course Dialog] Course Saved',
+
+    LessonsPageRequested = '[Course Landing Page] Lessons Page Requested',
+    LessonsPageLoaded = '[Courses API] Lessons Page Loaded',
+    LessonsPageCancelled = '[Courses API] Lessons Page Cancelled',
 }
 
 //single course actions
@@ -25,7 +31,6 @@ export class CourseLoaded implements Action {
 //all courses actions
 export class AllCoursesRequested implements Action {
     readonly type = CourseActionTypes.AllCoursesRequested;
-    constructor(public payload: any = null) { }
 }
 
 export class AllCoursesLoaded implements Action {
@@ -38,9 +43,34 @@ export class CourseSaved implements Action {
     constructor(public payload: { course: Update<Course> }) { }
 }
 
+//all lessons actions
+export interface PageQuery {
+    pageIndex: number;
+    pageSize: number;
+}
+
+export class LessonsPageRequested implements Action {
+    readonly type = CourseActionTypes.LessonsPageRequested;
+    constructor(public payload: { courseId: number, page: PageQuery }) { }
+}
+
+
+export class LessonsPageLoaded implements Action {
+    readonly type = CourseActionTypes.LessonsPageLoaded;
+    constructor(public payload: { lessons: Lesson[] }) { }
+}
+
+export class LessonsPageCancelled implements Action {
+    readonly type = CourseActionTypes.LessonsPageCancelled;
+}
+
+
 export type CourseActions =
-    CourseRequested 
-    | CourseLoaded 
+    CourseRequested
+    | CourseLoaded
     | AllCoursesRequested
     | AllCoursesLoaded
-    | CourseSaved;
+    | CourseSaved
+    | LessonsPageRequested
+    | LessonsPageLoaded
+    | LessonsPageCancelled;
